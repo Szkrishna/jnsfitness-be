b99ab12d01bc80febdbe038eca410784f51e54d6
yuvraj <singhrajneeshsvm@gmail.com>
2026-02-05 00:06:37 +0530

    fix the table name

--- src/models/gympage.model.js
+++ src/models/gympage.model.js
@@ -3,7 +3,7 @@ import db from '../config/dbConfiguration.js';
 
 const  gymPageDetails= (data,callback)=>{
     const{name,phone,membership,message} = data;
-    const sql =` INSERT INTO badminton_plan (name,phone, membership,comment)
+    const sql =` INSERT INTO fitness_table (name,phone, membership,comment)
     VALUES (?, ?, ?, ?) `;
     db.query(sql,[name,phone,membership,message],callback);
 };
